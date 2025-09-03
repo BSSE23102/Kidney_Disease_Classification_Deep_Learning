88cc24a7b080810b252ad4a2b305db39336b702a
BSSE23102 <143380730+BSSE23102@users.noreply.github.com>
2025-09-03 18:18:15 +0500

    main file

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-from src.cnnClassifier import logger
+import sys, os
+sys.path.append(os.path.abspath("src"))
 
-logger.info("Starting the project")
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionPipeline
+
+
+try:    
+    logger.info(">>>>> stage 1 started <<<<<")
+    obj = DataIngestionPipeline()
+    obj.main()
+    logger.info(">>>>> stage 1 completed <<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
